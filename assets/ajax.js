4a72302fe4e02ebe9ad5ec4423b99f8b5c0579fb
Amperus <73357067+lutzmaertin@users.noreply.github.com>
2022-12-25 19:58:16 +0100

    css

---
+++ assets/ajax.js
@@ -0,0 +1,41 @@
+$(function (){
+
+    $("div#output-1").load("./content/home.html");
+ 
+    $(".home").click(function(){
+    $("div#output-1").load("./content/home.html"); 
+        }) 
+
+    $(".webdesign").click(function(){
+    $("div#output-1").load("./content/webdesign.html"); 
+                 }) 
+
+    
+
+    $(".reisefotografie").click(function(){
+    $("div#output-1").load("./content/reisefotografie.html"); 
+         }) 
+     
+
+    $(".kontakt").click(function(){
+    $("div#output-1").load("./content/kontakt.html");
+         })
+
+
+    $(".impressum").click(function(){
+    $("div#output-1").load("./content/impressum.html");
+          }) 
+          
+          
+    $(".datenschutz").click(function(){
+    $("div#output-1").load("./content/datenschutz.html");
+          })       
+    
+     
+
+
+
+    
+
+
+}); 
